57d5726aaf3d97ff49381b8708c7851a31885461
Andriamasy Eric Mathieu <m.andriamasy@acsesante.fr>
2026-01-16 22:23:47 +0300

    feat : redeploye

--- app/layout.tsx
+++ app/layout.tsx
@@ -121,6 +121,7 @@ export const metadata: Metadata = {
   },
   category: 'Portfolio Professionnel',
   classification: 'Développeur Web Full-Stack',
+  
   verification: {
     google: 'SSv_6goMdeeDIa4EebKqfeNBth8GDRQmOdWml6',
   },
