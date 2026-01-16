1903888c0a7e233314b7c525e170c14c0b4da803
Andriamasy Eric Mathieu <m.andriamasy@acsesante.fr>
2026-01-16 23:06:53 +0300

    fix : corriger sitemap.xml

---
+++ app/sitemap.xml/route.ts
@@ -0,0 +1,21 @@
+import { NextResponse } from 'next/server'
+
+export async function GET() {
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<url>
+<loc>https://andriamasy.vercel.app/</loc>
+<lastmod>2026-01-16</lastmod>
+<changefreq>weekly</changefreq>
+<priority>1.0</priority>
+</url>
+</urlset>`
+
+  return new NextResponse(sitemap, {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/xml; charset=utf-8',
+      'Cache-Control': 'public, max-age=3600, s-maxage=3600',
+    },
+  })
+}
